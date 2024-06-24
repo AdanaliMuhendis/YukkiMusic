e6d2b49f143d15e3012071468384cacdd67ac99e
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-06-25 01:20:11 +0300

    Update config.py
    Signed-off-by: AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>

--- config/config.py
+++ config/config.py
@@ -53,7 +53,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/AdanaliMuhendis/ExtraPlugin",
+    "https://github.com/TheTeamVivek/ExtraPlugin",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
