a8599a10bd820a9c8f8c0bc56b9605577b96a3e2
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-07-01 15:23:57 +0300

    Update start.py
    Signed-off-by: AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -102,7 +102,7 @@ def alive_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="✿︎ ᴀᴅᴅ ᴍᴇ ✿︎", url=f"https://t.me/{app.username}?startgroup=true"
+                text="✚ Beni Grubuna Ekle", url=f"https://t.me/{app.username}?startgroup=true"
             ),
             InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
         ],
