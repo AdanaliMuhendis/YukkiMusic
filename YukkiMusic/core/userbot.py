35b6ae1ce740933fe5cb429f46b6765bfe719c03
AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>
2024-07-01 15:03:56 +0300

    Update userbot.py
    Signed-off-by: AdanaliMuhendis <134815100+AdanaliMuhendis@users.noreply.github.com>

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -65,7 +65,7 @@ class Userbot(Client):
                 pass
             assistants.append(1)
             try:
-                await self.one.send_message(config.LOG_GROUP_ID, "Assistant Started")
+                await self.one.send_message(config.LOG_GROUP_ID, "Yαყıɳԃαყıɱ 🌈...")
             except:
                 LOGGER(__name__).error(
                     f"Assistant Account 1 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
